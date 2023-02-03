ecb3b22430cd4d55d5d34cfd2eee32bfe3378a7b
daniel1718 <jbedoyachavarriaga@gmail.com>
2023-02-03 15:26:13 -0500

    eliminé algunas relaciones de sql

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -2,16 +2,6 @@ const { Paises, Personas, Recervas, Tarifas, Tikets } = require("./index");
 
 const initModels = () => {
 
-  Recervas.belongsTo(Paises, { as: "recerva_paisOrigen", foreignKey: "id_pais_origen" });
-  Paises.hasMany(Recervas, { as: "paisOrigen", foreignKey: "id_pais_origen" });
-
-  Recervas.belongsTo(Paises, { as: "recerva_paisDestino", foreignKey: "id_pais_destino" });
-  Paises.hasMany(Recervas, { as: "paisDestino", foreignKey: "id_pais_destino" });
-
-
-  Personas.belongsTo(Paises, {as: "pais_recidencia", foreignKey: "id_pais_recidencia"});
-  Paises.hasMany(Personas, {as: "recidencia", foreignKey: "id_pais_recidencia"});
-
   Tikets.belongsTo(Personas, {as: "persona", foreignKey: "id_personas"});
   Personas.hasMany(Tikets, {as: "tiket_persona", foreignKey: "id_personas"});
 
